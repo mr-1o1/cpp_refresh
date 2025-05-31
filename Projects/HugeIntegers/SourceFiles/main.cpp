05b3457427b042b58c0ece64a821fe529cf4bceb
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-05-31 17:27:44 +0300

    Add two functions to read huge integers and write huge integers

--- Projects/HugeIntegers/SourceFiles/main.cpp
+++ Projects/HugeIntegers/SourceFiles/main.cpp
@@ -2,9 +2,31 @@
 #include "HugeInteger.h"
 using namespace std;
 
+void readAllHugeIntegers(HugeInteger* &Hs, int &totalInts) {
+    ifstream Rdr("SourceFiles/Hs.txt");
+
+    Rdr >> totalInts;
+    cout << "Total Huge Integers: " << totalInts << endl;
+
+    Hs = new HugeInteger[totalInts];
+
+    for (int i = 0; i < totalInts; i++) {
+        Rdr >> Hs[i];
+    }
+}
+
+void showAllHugeIntegers(const HugeInteger* Hs, const int &totalInts) {
+    for (int i = 0; i < totalInts; i++) {
+        cout << Hs[i] << endl;
+    }
+}
+
 int main() {
-    cout << "Hello, it's me!" << endl;
-    HugeInteger H;
+    HugeInteger* Hs;
+    int totalInts;
+
+    readAllHugeIntegers(Hs, totalInts);
+    showAllHugeIntegers(Hs, totalInts);
 
     return 0;
 }
