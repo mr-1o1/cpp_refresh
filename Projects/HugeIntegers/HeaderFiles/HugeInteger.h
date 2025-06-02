46d79986ed29ade202201b612910e082ee6221e2
Hanzla <102576978+mr-1o1@users.noreply.github.com>
2025-06-02 20:49:51 +0300

    Add declaration for operator overloading functions

--- Projects/HugeIntegers/HeaderFiles/HugeInteger.h
+++ Projects/HugeIntegers/HeaderFiles/HugeInteger.h
@@ -5,9 +5,6 @@ using namespace std;
 #pragma once
 
 class HugeInteger {
-    
-
-public:
     int* Ds; // To store all the digits
     int totalDigits; // Represents total number of digits
     bool isNeg; // +ve or -ve number
@@ -23,14 +20,33 @@ public:
      */
     const HugeInteger elementWiseAdd(const HugeInteger &H) const;
     const HugeInteger elementWiseSubtract(const HugeInteger &H) const;
+    bool lessThanElementWise(const HugeInteger &H) const;
+    bool greaterThanElementWise(const HugeInteger &H) const;
+    bool equalElementWise(const HugeInteger &H) const;
+    void trim();
 
+    int greaterDigitCount(const HugeInteger &H) const;
 public:
+
     HugeInteger();
     HugeInteger(ifstream &Rdr);
     HugeInteger(const HugeInteger&); // copy constructor
     HugeInteger(const int);
 
     const HugeInteger& operator=(const HugeInteger&);
+    int operator[](int index) const;
+
+    const HugeInteger operator+(const HugeInteger&) const;
+    const HugeInteger operator+=(const HugeInteger&);
+    const HugeInteger operator-() const;
+    const HugeInteger operator-(const HugeInteger&) const;
+    const HugeInteger operator*(const HugeInteger&) const;
+
+    bool operator<(const HugeInteger &H) const;
+    bool operator>(const HugeInteger &H) const;
+    bool operator==(const HugeInteger &H) const;
+    const HugeInteger operator++(int);
+
 
     void setInteger(const HugeInteger&); // for deep copy
     void setInteger(ifstream &Rdr);
